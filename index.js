a19570c22a009ff7de47859bd700371987a9d7c7
Billy F <willfira717@gmail.com>
2020-10-29 11:04:16 -0400

    commit tree 02f8c9dd6d2f95a3258423d6c6c04866f6aa23e1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+// const giftGivers = ['Lisa', 'Kaitlyn', 'Jan'];
+// const holiday = ['surprise'];
+
+function writeCards(x, y) {
+    newArray = []
+    for (let i = 0; i < x.length; i++) {
+        newArray.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`);
+    }
+
+    return newArray;
+
+};
+
+writeCards(['Lisa', 'Kaitlyn', 'Jan'], 'surpise');
